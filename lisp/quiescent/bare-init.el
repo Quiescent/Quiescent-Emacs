21ca19cae36f9e5058c259a6da81c3a4370a505b
Edward John Steere <edward.steere@gmail.com>
2020-07-06 10:14:06 +0200

    Add dependencies of org-brain export to bare-init

--- lisp/quiescent/bare-init.el
+++ lisp/quiescent/bare-init.el
@@ -13,7 +13,7 @@
 (load-file "~/.emacs.d/conf/system-vars.el")
 (if (file-exists-p "~/.emacs.d/conf/system-conf.el")
     (load-file "~/.emacs.d/conf/system-conf.el")
-    (warn "No system-conf.el file exists!  Please configure variables in ~/.emacs.d/conf/system-vars.el and put them in ~/.emacs.d/conf/system-conf.el"))
+  (warn "No system-conf.el file exists!  Please configure variables in ~/.emacs.d/conf/system-vars.el and put them in ~/.emacs.d/conf/system-conf.el"))
 
 (package-initialize)
 (package-refresh-contents)
@@ -23,6 +23,12 @@
   (package-install 'use-package-chords nil))
 
 (require 'use-package-chords)
+
+;; Required for org-brain export.  It's not a proper package yet.
+(use-package xmlgen
+  :ensure t)
+(use-package a
+  :ensure t)
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
