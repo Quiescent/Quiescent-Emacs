a03df302920a02d57ba71f770b82ad13b20eef0f
Edward John Steere <edward.steere@gmail.com>
2020-08-23 11:28:09 +0200

    Don't show messages when auto saving

--- custom.el
+++ custom.el
@@ -12,6 +12,7 @@
  '(LaTeX-command "latex")
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
+ '(auto-save-no-message t)
  '(avy-keys '(49 50 51 52 53 54 55 56 57 48))
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(completion-styles '(flex basic partial-completion emacs22))
