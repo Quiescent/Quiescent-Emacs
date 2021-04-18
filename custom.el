6b50a58eefc51769fe98a598619858b0a77de328
Edward John Steere <edward.steere@gmail.com>
2021-04-18 10:07:40 +0200

    Use fringe and message for alert

--- custom.el
+++ custom.el
@@ -10,6 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex")
+ '(alert-default-style 'fringe-message)
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(auto-save-no-message t)
