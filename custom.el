c568232d13b9d596b96089fa950bf11e3ebb8f1d
Edward Steere <edward.steere@gmail.com>
2022-08-12 15:07:05 +0200

    Make unknown references in `js2-mode' bold

--- custom.el
+++ custom.el
@@ -77,7 +77,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-external-variable ((t (:foreground "orange2"))))
+ '(js2-external-variable ((t (:foreground "orange2" :weight bold))))
  '(js2-highlight-vars-face ((t (:underline t))))
  '(js2-highlight-vars-second-face ((t (:underline "white"))))
  '(org-agenda-clocking ((t (:inherit nano-face-strong))))
