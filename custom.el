73662806c4bfb262aca2418484001a13a42b381e
Edward John Steere <edward.steere@gmail.com>
2022-08-14 10:00:20 +0200

    Tweak variable and fixed-pitch styles

--- custom.el
+++ custom.el
@@ -77,11 +77,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family "Roboto"))))
  '(js2-external-variable ((t (:foreground "orange2" :weight bold))))
  '(js2-highlight-vars-face ((t (:underline t))))
  '(js2-highlight-vars-second-face ((t (:underline "white"))))
  '(org-agenda-clocking ((t (:inherit nano-face-strong))))
  '(org-agenda-done ((t (:inherit nano-face-faded :foreground "#d0d8dc"))))
+ '(org-level-1 ((t (:inherit nano-face-strong :extend t :height 1.6))))
+ '(org-level-2 ((t (:inherit nano-face-strong :extend t :height 1.3))))
+ '(org-level-3 ((t (:inherit nano-face-strong :extend t :height 1.3))))
+ '(org-level-4 ((t (:inherit nano-face-strong :extend t :height 1.3))))
+ '(org-level-5 ((t (:inherit nano-face-strong :extend t :height 1.3))))
+ '(org-level-6 ((t (:inherit nano-face-strong :extend t :height 1.3))))
+ '(org-level-7 ((t (:inherit nano-face-strong :extend t :height 1.3))))
+ '(org-level-8 ((t (:inherit nano-face-strong :extend t :height 1.3))))
  '(org-pomodoro-mode-line ((t (:foreground "#ff6c6b"))))
  '(org-scheduled-previously ((t (:inherit nano-face-strong))))
  '(org-scheduled-today ((t (:inherit nano-face-default))))
@@ -94,7 +103,7 @@
  '(rainbow-delimiters-depth-7-face ((t (:foreground "dark slate blue"))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "navy"))))
  '(tide-hl-identifier-face ((t (:inherit nil :underline t))))
- '(variable-pitch ((t (:background "#2E3440" :foreground "#ECEFF4" :weight light :height 140 :family "Noto"))))
+ '(variable-pitch ((t (:family "Roboto"))))
  '(visible-mark-face1 ((t (:background "slate gray")))))
 
 (provide 'custom)
