c047e1f5fdade6f98169d781a3d220dc6543f8e1
Edward John Steere <edward.steere@gmail.com>
2026-05-29 10:25:44 +0200

    Fix the width of the lisp repl window

--- init.el
+++ init.el
@@ -3279,7 +3279,7 @@ arguments actually mean."
                 display-buffer-in-side-window)
                (reusable-frames . t)
                (side . left)
-               (window-width . 0.5)))
+               (window-width . 100)))
 
 ;; Rustic Compilation
 (add-to-list 'display-buffer-alist
